f53ff1c85dc056b58896a2be1ef99381cad05f83
packjjo <packjjo@gmail.com>
2021-01-25 21:44:37 +0900

    the end of the component event

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -9,7 +9,13 @@ class TOC extends Component{
       console.log(data.length);
       while(i < data.length){
         lists.push(<li key={data[i].id}>
-          <a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+          <a href={"/content/"+data[i].id}
+          data-id={data[i].id}
+          onClick = {function(e){
+            e.preventDefault();
+            this.props.onChangePage(e.target.dataset.id);
+          }.bind(this)}>
+            {data[i].title}</a></li>);
         i = i + 1;
       }
       return(
